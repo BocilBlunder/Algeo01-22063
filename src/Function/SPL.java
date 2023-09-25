559a6f52f7dfd87ed0c8c11cfd3a89e27b945720
shazyaaa <shazyaaudrea@gmail.com>
2023-09-25 21:47:28 +0700

    modified:   SPL.java

--- src/Function/SPL.java
+++ src/Function/SPL.java
@@ -1,8 +1,6 @@
 package Function;
 
 import ADTMatrix.Matrix;
-import java.util.*;
-import java.io.*;
 
 public class SPL {
     public static void inversSPL (Matrix MatrixSPL){
@@ -10,6 +8,7 @@ public class SPL {
         Matrix B;
         Matrix x;
         Matrix matrixInvers;
+        int i, j;
 
         if ((MatrixSPL.getRowLength() != MatrixSPL.getColLength()-1) || Determinan.detKofaktor(MatrixSPL) == 0){
             System.out.print("Matrix tidak memiliki balikan.");
@@ -20,11 +19,23 @@ public class SPL {
         B = new Matrix(MatrixSPL.col, 1);
         x = new Matrix(MatrixSPL.getRowLength(), 1);
 
+        for (i = 0; i < MatrixSPL.getRowLength(); i++){
+            for (j = 0; j < MatrixSPL.getColLength()-1; j++){
+                A.setElmt(i, j, MatrixSPL.getElmt(i, j));
+            }
+        }
+
+        for (i = 0; i < MatrixSPL.getRowLength(); i++){
+            for (j = MatrixSPL.getColLength()-1; j < MatrixSPL.getColLength(); j++){
+                B.setElmt(i, j, A.getElmt(i, j));
+            }
+        }
+
         matrixInvers = Invers.inversIdentitas(A);
 
         x = Matrix.multiplyMatrix(matrixInvers, B);
         
-        for(int i = 0; i < x.getRowLength(); i++){
+        for(i = 0; i < x.getRowLength(); i++){
             System.out.print("x"+Integer.toString(i+1)+" = "+Double.toString(x.getElmt(i, 0))+"\n");
         }
     }
