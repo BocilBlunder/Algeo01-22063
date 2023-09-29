b050431469fe5c2de709ba128f6d325d30e4a77a
William Glory <williamgloryh@gmail.com>
2023-09-30 00:54:44 +0700

    final regresi

--- src/Function/Main.java
+++ src/Function/Main.java
@@ -6,7 +6,6 @@ public class Main{
     public static void main (String[] args){
         double[][]m = InputMatrix.readRegresiKeyboard();
         Matrix m1 = new Matrix(m, m.length, m[0].length);
-        double hasil = Matrix.sumMultiplyCol(m1, 1, 2);
-        System.out.print(hasil);
+        Regresi.regresiLinear(m1);
     }
 }
--- src/Function/Regresi.java
+++ src/Function/Regresi.java
@@ -3,12 +3,23 @@ import ADTMatrix.*;
 
 public class Regresi {
     public static void regresiLinear (Matrix m){
-        Matrix mTemp;
         int i, j;
         double temp = 0;
-        Matrix MRes = new Matrix(m.col, m.col + 1);
-        for(i = 0 ; i < MRes.row ; i++){
-            for(j = 0 ; j < MRes.col ; j++){
+        double result = 0;
+        double sum = 0;
+        Matrix mTemp;
+        double [] x;
+
+        x = new double [m.col - 1];
+        System.out.println("Masukkan nilai x yang ingin ditaksir: ");
+        for(i = 0; i < x.length; i++){
+            x[i] = InputMatrix.input.nextDouble();
+        }
+
+        mTemp = new Matrix(m.col, m.col + 1);
+
+        for(i = 0 ; i < mTemp.row ; i++){
+            for(j = 0 ; j < mTemp.col ; j++){
                 if(i == 0 && j == 0){
                     temp = m.row;
                     
@@ -22,12 +33,42 @@ public class Regresi {
                 else if (i > 0 && j > 0) {
                     temp = Matrix.sumMultiplyCol(m, i-1, j-1);
                 }
-                MRes.setElmt(i, j, temp);
+                mTemp.setElmt(i, j, temp);
             }
         }
-        MRes = Matrix.gaussElimination(MRes);
-        double[] m1 = new double [MRes.getRowLength()];
-        Matrix.backSubstitution(MRes, m1); 
+        
+        mTemp = Matrix.gaussElimination(mTemp);
+        double[] m1 = new double [mTemp.getRowLength()];
+        Matrix.backSubstitution(mTemp, m1); 
+
+        System.out.print("f(x) = ");
+        for (i = 0; i < mTemp.row; i++) {
+            result = m1[i];
+            sum += result;
+            if (i == 0){
+                if (m1[i] > 0){
+                    System.out.printf("+ %.4f ", m1[i]);
+                } else {
+                    m1[i] *= -1;
+                    System.out.printf("- %.4f ", m1[i]);
+                }
+            } else if ( i > 0 && i < mTemp.row - 1){
+                if (m1[i] > 0){
+                    System.out.printf("+ %.4fx%d ", m1[i], i);
+                } else {
+                    m1[i] *= -1;
+                    System.out.printf("- %.4fx%d ", m1[i], i);
+                }
+            } else if (i == mTemp.row - 1){
+                if (m1[i] > 0){
+                    System.out.printf("+ %.4fx%d, ", m1[i], i);
+                } else {
+                    m1[i] *= -1;
+                    System.out.printf("- %.4fx%d, ", m1[i], i);
+                }
+            }
+        }
+        System.out.printf("f(xk) = %.4f", sum);
     }
  }
 
